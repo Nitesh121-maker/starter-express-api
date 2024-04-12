64ea5541eec53818026ad1a9d03588bb9c55d345
Nitesh Chauhan <72297535+Nitesh121-maker@users.noreply.github.com>
2024-04-12 16:40:34 +0530

    Update index.js1

--- index.js
+++ index.js
@@ -295,6 +295,6 @@ app.get('/clientdata',  (req, res,) => {
   });
 
 
-app.listen(3002,() => {
+app.listen(process.env.PORT || 3000,() => {
     console.log("Server is listening on port 3002. Ready for connections.");
 });
